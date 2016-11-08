5ad3509e119ed4d16c96c97f8f462410662f0460
Javier Rotger Lebrón <fjrotgerl@gmail.com>
2016-11-08 21:28:04 +0100

    Cream nova classe amb funcio hola

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        printHola();
-    }
-    public static void printHola(){
-        System.out.println("Hola món!");
+        printHola.printHolaFuncio();
     }
 }
---
+++ src/com/company/printHola.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by fjrotgerl on 08/11/16.
+ */
+public class printHola {
+    public static void printHolaFuncio(){
+        System.out.println("Hola món!");
+    }
+}
