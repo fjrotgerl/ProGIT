a32f5ecf11e64877fe7767ecce651c194e340471
Javier Rotger Lebrón <fjrotgerl@gmail.com>
2016-11-09 21:36:02 +0100

    Afegim un for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         printHola.printHolaFuncio();
+        int c = 0;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Número " + c);
+            c++;
+        }
     }
 }
