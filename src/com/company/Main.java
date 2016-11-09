1e844f3a44e7f35567d20b581fed592bf399a3af
Javier Rotger Lebrón <fjrotgerl@gmail.com>
2016-11-09 21:50:09 +0100

    Afegim comentaris i array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        //Print "Hola mon!"
         printHola.printHolaFuncio();
+
+        //Llista del 0 fins el 10
         int c = 0;
         for (int i = 0; i <= 10; i++) {
             System.out.println("Número " + c);
             c++;
         }
+
+        //Print d'array
+        String[] ar = {"Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte","Diumenge"};
+        System.out.println(Arrays.toString(ar));
     }
 }
